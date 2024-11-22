46fa63be4ff16d76c6ec79a08f54a0ae6d1ddb1a
zacken-source <marino.chereaudemedina@ynov.com>
2024-11-22 14:15:10 +0100

    nouveaux templates

--- hangmanClassic/hangman/logic.go
+++ hangmanClassic/hangman/logic.go
@@ -10,22 +10,26 @@ import (
 // verifie si la lettre proposée l'a déjà été et rajout dans lettertested (renvoie true ou false)
 func (s *Structure) VerifLetter(letter []rune) bool { //-------------------------true =
 	isHere := true //lettre pas dans lkes lettre testée
+
 	for _, i := range s.LetterTested {
 		if i == letter[0] {
 			isHere = false //lettre dans les lettres testée
 		}
 	}
+
 	if isHere && len(s.LetterTested) == 0 { // si lettre pas dans les lettres testée
 		s.LetterTested += string(letter)
 	} else if isHere {
 		s.LetterTested += ", " + string(letter)
 	}
+
 	return isHere
 }
 
 // verifie si la lettre est dans le mot et le remplace dans le bon cas (renvoie true ou false)
 func (s *Structure) CheckLetter(letter []rune) bool {
 	isHere := false
+
 	for _, l := range letter {
 		for index, r := range s.SecretWord {
 			if l == r {
@@ -34,34 +38,38 @@ func (s *Structure) CheckLetter(letter []rune) bool {
 			}
 		}
 	}
-	if !s.VerifLetter(letter){
+
+	if !s.VerifLetter(letter) {
 		isHere = true
 	}
+
 	return isHere
 }
 
 func (s *Structure) ConvertRinS(runes []rune) string {
 	var chaine string = ""
+
 	for _, i := range runes {
 		chaine += string(i)
 	}
+
 	return chaine
 }
 
 // verifie si le mot est le bon
 func (s *Structure) CheckWord(letter []rune) bool {
 	var isHere bool = true
+
 	for _, l := range letter {
 		if !s.CheckLetter([]rune{l}) {
 			isHere = false
 		}
-
 	}
 
 	if isHere {
 		s.Win = true
-
 	}
+
 	return isHere
 }
 
@@ -69,22 +77,27 @@ func (s *Structure) CheckWord(letter []rune) bool {
 func (s *Structure) SelectRandomWord() []rune {
 	rand.Seed(time.Now().UnixNano())
 	content, err := os.ReadFile("texte/dictionnaire.txt")
+
 	if err != nil {
 		return nil
 	}
+
 	ListOfWord := strings.Split(string(content), "\n")
 	randomIndex := rand.Intn(len(ListOfWord))
 	str := ListOfWord[randomIndex]
 	word := str[:len(str)-1]
+
 	return []rune(word)
 }
 
 // Initialiser les blancs
 func (s *Structure) InitializeBlanks() []rune {
 	blanks := make([]rune, len(s.SecretWord))
+
 	for i := range blanks {
 		blanks[i] = '_'
 	}
+
 	return blanks
 }
 
@@ -98,12 +111,10 @@ func (s *Structure) CheckOut() {
 	var count = len(s.SecretWord)
 	var count2 int = 0
 
-	//ya una couillasse ici, ouais genre là au secoursdkchzodcnazpdcnadbajbajdbvekjvkejveffvevkevdbjkdekbnvdknevdn,k,nkvk,n
 	for index, i := range s.SecretWord {
 
 		if s.Blanks[index] == rune(i) {
 			count2 += 1
-
 		}
 	}
 
--- hangmanClassic/hangman/web.go
+++ hangmanClassic/hangman/web.go
@@ -49,43 +49,29 @@ func (s *Structure) win(w http.ResponseWriter, r *http.Request) {
 
 }
 func (s *Structure) game(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("debut")
 
 	if r.Method == "POST" {
 		r.ParseForm()
 		letter := r.PostFormValue("letter")
-		fmt.Println(letter, " --------------> la letter ou mot qu'on recoit")
-
 		s.Letter = []rune(letter)
-		fmt.Println(s.Letter, " --------------> la letter ou mot qu'on recoit en rune")
-
 		var check bool = false
-		fmt.Println("avant")
 
 		if len(s.Letter) == 1 {
-			fmt.Println("first")
-
 			if s.CheckLetter(s.Letter) {
-				fmt.Println("1")
 				check = true
 			}
-
 		} else if len(s.Letter) == len(s.SecretWord) {
-			fmt.Println("2")
-
 			check = s.CheckWord(s.Letter)
 		} else if len(s.Letter) == 0 {
-			fmt.Println("3")
 			check = true
 		}
-		fmt.Println("derbierr")
+
 		if !check {
-			fmt.Println("perdu")
 			s.Lives -= 1
 		}
-		fmt.Println("a checkout")
+
 		s.CheckOut()
-		fmt.Println("ap checkout")
+
 		web := DataForm{
 			Motsecret:       s.ConvertRinS(s.SecretWord),
 			Motcachee:       s.ConvertRinS(s.Blanks),
@@ -94,13 +80,6 @@ func (s *Structure) game(w http.ResponseWriter, r *http.Request) {
 			Essaies:         s.Lives,
 			Echec:           s.Lose,
 		}
-		fmt.Println("----------------deuxieme atemp")
-		fmt.Println(web.Motsecret)
-		fmt.Println(web.Motcachee)
-		fmt.Println(web.LettresUtilisee)
-		fmt.Println(web.Victoire)
-		fmt.Println(web.Essaies)
-		fmt.Println(web.Echec)
 
 		tmpl := template.Must(template.ParseFiles("HtmlCss/game.html"))
 		tmpl.Execute(w, web)
@@ -113,13 +92,6 @@ func (s *Structure) game(w http.ResponseWriter, r *http.Request) {
 			Essaies:         s.Lives,
 			Echec:           s.Lose,
 		}
-		fmt.Println("--------------premier attemp")
-		fmt.Println(web.Motsecret)
-		fmt.Println(web.Motcachee)
-		fmt.Println(web.LettresUtilisee)
-		fmt.Println(web.Victoire)
-		fmt.Println(web.Essaies)
-		fmt.Println(web.Echec)
 
 		tmpl := template.Must(template.ParseFiles("HtmlCss/game.html"))
 		tmpl.Execute(w, web)
